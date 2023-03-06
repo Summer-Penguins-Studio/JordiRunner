56e43254ab2689c85eff673fdc5bf4c3951ba96f
Carlos Daniel Robaina Rivero <cdrobayna01@gmail.com>
2023-03-06 02:22:45 -0500

    Trabaja puto

---
+++ Assets/Scripts/World/WorldMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float groundSpeed;
+    [SerializeField]
+    private float limitPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-groundSpeed, 0, 0);
+    }
+}
